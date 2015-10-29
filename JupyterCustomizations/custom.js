6faf71c25584adaa79ce0650da384273f1ebea13
Ben Varkey Benjamin <benvarkey@pulsar>
2015-10-29 01:54:56 -0700

    -Updated customizations for Jupyter 4

--- JupyterCustomizations/custom.js
+++ JupyterCustomizations/custom.js
@@ -1,6 +1,6 @@
 /**
  *
- * This file contains javascript extensions for IPython notebook,
+ * This file contains javascript extensions for Jupyter notebook,
  * compatible with Jupyter. The file contains the following extensions
  *
  * a. Button to launch QTConsole
@@ -9,12 +9,17 @@
  * d. Hide all marked cells when Notebook is launched
  * e. Open gVim to edit current cell and update with saved content
  * f. Increase/Decrease input code size
+ * g. Show line numbers by deafult
  *
  * Other settings:
  * i.  Show line numbers by default in code cells
  * ii. Show equation number when using Mathjax
  *
- * Ben Varkey Benjamin : 21-Mar-2015
+ * Ben Varkey Benjamin :
+ * 21-Mar-2015 : First version
+ * 16-Apr-2015 : Added +/- for input code size
+ * 29-Oct-2015 : Remove code-folding (available in nbextensions)
+ *               Updated to work with Jupyter 4
  */
 
 /*
@@ -36,7 +41,7 @@ function code_change_fontsize(doIncrease) {
     var pre_css = null;
     var pre_style = null;
     for(i = 0; i < document.styleSheets.length; i++){
-        if(/localhost.*\/static\/custom\/custom\.css/.test(document.styleSheets[i].href)){ //if style sheet is custom.css
+        if(/localhost.*\/custom\/custom\.css/.test(document.styleSheets[i].href)){ //if style sheet is custom.css
             pre_css = document.styleSheets[i]; //pre_css now contains the style sheet custom.css
             break;
         }
@@ -67,12 +72,12 @@ function code_change_fontsize(doIncrease) {
     pre_style.fontSize = font_size + "px";
 }
 
-require([
+define([
     'base/js/namespace',
     'base/js/events'
-    ], function(IPython, events) {
+    ], function(Jupyter, events) {
         events.on("app_initialized.NotebookApp", function () {
-            IPython.toolbar.add_buttons_group([
+            Jupyter.toolbar.add_buttons_group([
                 /**
                  * a. Button to launch QTConsole
                  */
@@ -80,7 +85,7 @@ require([
                      'label'   : 'Run QTConsole',
                      'icon'    : 'fa-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
                      'callback': function () {
-                         IPython.notebook.kernel.execute('%qtconsole')
+                         Jupyter.notebook.kernel.execute('%qtconsole')
                      }
                 },
                 /*
@@ -94,7 +99,7 @@ require([
                      }
                 },
                 /*
-                 * g. Button to increase/decrease code font size
+                 * f. Button to increase/decrease code font size
                  */
                 {
                      'label'   : 'Increase code font size',
@@ -115,7 +120,7 @@ require([
             /*
              * c. Toolbar entry to add show/hide checkbox for each cell
              */
-            var CellToolbar = IPython.CellToolbar;
+            var CellToolbar = Jupyter.CellToolbar;
             var show_hide_input = CellToolbar.utils.checkbox_ui_generator('Hide Input',
              // setter
              function(cell, value){
@@ -145,34 +150,33 @@ require([
              * I think I found this code on some Stackoverflow page.
              *
              */
-            IPython.keyboard_manager.command_shortcuts.add_shortcut('g', {
+            Jupyter.keyboard_manager.command_shortcuts.add_shortcut('g', {
                 handler : function (event) {
-                    var input = IPython.notebook.get_selected_cell().get_text();
+                    var input = Jupyter.notebook.get_selected_cell().get_text();
                     var cmd = "f = open('/tmp/.ipycell.py', 'w');f.close()";
                     if (input != "") {
                         cmd = '%%writefile /tmp/.ipycell.py\n' + input;
                     }
-                    IPython.notebook.kernel.execute(cmd);
+                    Jupyter.notebook.kernel.execute(cmd);
                     cmd = "import os;os.system('gvim /tmp/.ipycell.py')";
-                    IPython.notebook.kernel.execute(cmd);
+                    Jupyter.notebook.kernel.execute(cmd);
                     return false;
                 }}
             );
-            IPython.keyboard_manager.command_shortcuts.add_shortcut('u', {
+            Jupyter.keyboard_manager.command_shortcuts.add_shortcut('u', {
                 handler : function (event) {
                     function handle_output(msg) {
                         var ret = msg.content.text;
-                        IPython.notebook.get_selected_cell().set_text(ret);
+                        Jupyter.notebook.get_selected_cell().set_text(ret);
                     }
                     var callback = {'output': handle_output};
                     var cmd = "f = open('/tmp/.ipycell.py', 'r');print(f.read())";
-                    IPython.notebook.kernel.execute(cmd, {iopub: callback}, {silent: false});
+                    Jupyter.notebook.kernel.execute(cmd, {iopub: callback}, {silent: false});
                     return false;
                 }}
             );
-
-            // i. Show line numbers by default
-            IPython.CodeCell.options_default.cm_config["lineNumbers"] = true;
+            // g. Show line numbers by default
+            Jupyter.CodeCell.options_default.cm_config["lineNumbers"] = true;
         });
 });
 
@@ -182,7 +186,7 @@ require([
  * Ref: http://nbviewer.ipython.org/github/Carreau/posts/blob/master/04-initialisation-cell.ipynb
  */
 var hide_init = function(){
-    var cells = IPython.notebook.get_cells();
+    var cells = Jupyter.notebook.get_cells();
     for(var i in cells){
         var cell = cells[i];
         var namespace =  cell.metadata._ben|| {};
@@ -194,9 +198,4 @@ var hide_init = function(){
         }
     }
 };
-$([IPython.events]).on('notebook_loaded.Notebook', hide_init);
-
-// ii. Numbering for IPython notebook equations
-MathJax.Hub.Config({
-      TeX: { equationNumbers: { autoNumber: "AMS" } }
-});
+$([Jupyter.events]).on('notebook_loaded.Notebook', hide_init);
